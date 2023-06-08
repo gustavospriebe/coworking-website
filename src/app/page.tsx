e3713a9e50c96757e14eaaa4d43acf52cf185551
Gustavo Priebe <40157345+gustavospriebe@users.noreply.github.com>
2023-06-08 05:50:54 -0300

    header done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,17 @@
 "use client";
 
 import logo from "@/assets/logo.svg";
+import menu from "@/assets/menu.svg";
 import Image from "next/image";
 
 export default function Home() {
     return (
         <main>
-            <header className="h-20 w-full bg-bg-black px-9 py-6 sm:h-24 xl:h-28 xl:px-12">
-                <Image src={logo} alt="" className="h-full w-auto" />
+            <header className="flex h-20 w-full items-center justify-between bg-bg-black px-9 py-6 sm:h-24 lg:h-28 lg:px-12">
+                <Image src={logo} alt="" className="h-10 w-auto sm:h-12" />
+                <Image src={menu} alt="" className="h-8 w-auto lg:hidden" />
             </header>
+            <div></div>
         </main>
     );
 }
