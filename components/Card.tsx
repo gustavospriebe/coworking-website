6be95a0d2a8a369de5f70a9d141607319671ba8b
Gustavo Priebe <40157345+gustavospriebe@users.noreply.github.com>
2023-06-12 03:38:49 -0300

    left part of carousel almost done

--- components/Card.tsx
+++ components/Card.tsx
@@ -24,31 +24,147 @@ const notifications = [
 
 type CardProps = React.ComponentProps<typeof Card>;
 
-enum Highlights {
-    Unstyled = "1-18",
-    Composable = "20-37",
-    Customizable = "39-55",
-    Consistent = "57-74",
-}
+type Highlights = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
+
+const cards = [2, 3, 4, 5, 6, 7, 8];
 
 export function CardComponent({ className, ...props }: CardProps) {
-    const [activeHighlight, setActiveHighlight] = useState<Highlights>(
-        Highlights.Unstyled
-    );
+    const [activeHighlight, setActiveHighlight] = useState(1);
     return (
-        <Card
-            className={cn(
-                "cursor-pointer border-none  bg-[#ffffff0e] p-0 text-whiter shadow-md",
-                className
-            )}
-            {...props}
-        >
-            <CardHeader className="p-4">
-                <CardTitle className="font-bold">Notifications</CardTitle>
-                <CardDescription className="text-secundary">
-                    You have 3 unread messages.
-                </CardDescription>
-            </CardHeader>
-        </Card>
+        // scroll-m-0  overflow-y-hidden overflow-x-scroll
+        <div className="-mx-10 space-y-2 pl-5">
+            <Card
+                className={cn(
+                    "cursor-pointer border-none  p-0 text-whiter shadow-md",
+                    activeHighlight === 1 ? "card-active" : "card-ghost",
+                    className
+                )}
+                {...props}
+                onMouseDown={() => setActiveHighlight(1)}
+                onClick={() => setActiveHighlight(1)}
+            >
+                <CardHeader className="p-4">
+                    <CardTitle className="font-bold">Notifications</CardTitle>
+                </CardHeader>
+            </Card>
+            {cards.map((card, index) => (
+                <Card
+                    key={index}
+                    className={cn(
+                        "cursor-pointer border-none p-0 text-whiter shadow-md",
+                        activeHighlight === card ? "card-active" : "card-ghost",
+                        className
+                    )}
+                    {...props}
+                    onMouseDown={() => setActiveHighlight(card)}
+                    onClick={() => setActiveHighlight(card)}
+                >
+                    <CardHeader className="p-4">
+                        <CardTitle className="font-bold">
+                            Notifications
+                        </CardTitle>
+                    </CardHeader>
+                </Card>
+            ))}
+        </div>
     );
 }
+
+// <Card
+// className={cn(
+//     "cursor-pointer border-none p-0 text-whiter shadow-md",
+//     activeHighlight === 2 ? "card-active" : "card-ghost",
+//     className
+// )}
+// {...props}
+// onMouseDown={() => setActiveHighlight(2)}
+// onClick={() => setActiveHighlight(2)}
+// >
+// <CardHeader className="p-4">
+//     <CardTitle className="font-bold">Notifications</CardTitle>
+// </CardHeader>
+// </Card>
+// <Card
+// className={cn(
+//     "cursor-pointer border-none p-0 text-whiter shadow-md",
+//     activeHighlight === 3 ? "card-active" : "card-ghost",
+//     className
+// )}
+// {...props}
+// onMouseDown={() => setActiveHighlight(3)}
+// onClick={() => setActiveHighlight(3)}
+// >
+// <CardHeader className="p-4">
+//     <CardTitle className="font-bold">Notifications</CardTitle>
+// </CardHeader>
+// </Card>
+// <Card
+// className={cn(
+//     "cursor-pointer border-none p-0 text-whiter shadow-md",
+//     activeHighlight === 4 ? "card-active" : "card-ghost",
+//     className
+// )}
+// {...props}
+// onMouseDown={() => setActiveHighlight(4)}
+// onClick={() => setActiveHighlight(4)}
+// >
+// <CardHeader className="p-4">
+//     <CardTitle className="font-bold">Notifications</CardTitle>
+// </CardHeader>
+// </Card>
+// <Card
+// className={cn(
+//     "cursor-pointer border-none p-0 text-whiter shadow-md",
+//     activeHighlight === 5 ? "card-active" : "card-ghost",
+//     className
+// )}
+// {...props}
+// onMouseDown={() => setActiveHighlight(5)}
+// onClick={() => setActiveHighlight(5)}
+// >
+// <CardHeader className="p-4">
+//     <CardTitle className="font-bold">Notifications</CardTitle>
+// </CardHeader>
+// </Card>
+// <Card
+// className={cn(
+//     "cursor-pointer border-none p-0 text-whiter shadow-md",
+//     activeHighlight === 6 ? "card-active" : "card-ghost",
+//     className
+// )}
+// {...props}
+// onMouseDown={() => setActiveHighlight(6)}
+// onClick={() => setActiveHighlight(6)}
+// >
+// <CardHeader className="p-4">
+//     <CardTitle className="font-bold">Notifications</CardTitle>
+// </CardHeader>
+// </Card>
+// <Card
+// className={cn(
+//     "cursor-pointer border-none p-0 text-whiter shadow-md",
+//     activeHighlight === 7 ? "card-active" : "card-ghost",
+//     className
+// )}
+// {...props}
+// onMouseDown={() => setActiveHighlight(7)}
+// onClick={() => setActiveHighlight(7)}
+// >
+// <CardHeader className="p-4">
+//     <CardTitle className="font-bold">Notifications</CardTitle>
+// </CardHeader>
+// </Card>
+// <Card
+// className={cn(
+//     "cursor-pointer border-none p-0 text-whiter shadow-md",
+//     activeHighlight === 8 ? "card-active" : "card-ghost",
+//     className
+// )}
+// {...props}
+// onMouseDown={() => setActiveHighlight(8)}
+// onClick={() => setActiveHighlight(8)}
+// >
+// <CardHeader className="p-4">
+//     <CardTitle className="font-bold">Notifications</CardTitle>
+// </CardHeader>
+// </Card>
--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -10,7 +10,6 @@ import "react-awesome-slider/dist/styles.css";
 import { Separator } from "./ui/separator";
 import { CardComponent } from "./Card";
 
-
 const teste = [1, 2, 3, 4, 5, 6];
 
 export function CarouselComponent() {
@@ -29,19 +28,19 @@ export function CarouselComponent() {
 
     return (
         <div className="w-full bg-bg-black">
-            <div className="mx-7 flex h-screen max-w-6xl flex-col items-start gap-5 py-20 md:flex-row lg:gap-8">
+            <div className="mx-7 flex max-w-6xl flex-col items-start gap-5 py-16 md:flex-row lg:gap-8">
                 <div className="max-w-[420px]">
-                    <p className="text-green-secundary">Nossa Estrutura</p>
-                    <p className="mb-4 text-4xl text-whiter">Nossa Estrutura</p>
-                    <Separator className="my-2 w-4/5 text-whiter/20" />
-                    <p className="mb-4 text-base text-whiter">
+                    <p className="mb-2 text-green-secundary">Nossa Estrutura</p>
+                    <p className="mb-4 text-2xl text-whiter">
                         Conheça tudo que podemos oferecer
                     </p>
-                    <p className="max-w-[500px] text-secundary mb-6">
-                    Aqui sua empresa poderá ter um espaço comercial de qualidade, com custo reduzido e muitas outras vantagens.
+                    <Separator className="my-4 text-whiter/20" />
+                    <p className="mb-6 max-w-[500px] text-secundary">
+                        Aqui sua empresa poderá ter um espaço comercial de
+                        qualidade, com custo reduzido e muitas outras vantagens.
                     </p>
 
-                            <CardComponent />
+                    <CardComponent />
                     {/* <div className="-mx-5 w-screen scroll-m-0 overflow-y-hidden overflow-x-scroll pl-5  ">
                         <div className="flex gap-x-3 gap-y-3 sm:gap-x-5">
                         </div>
