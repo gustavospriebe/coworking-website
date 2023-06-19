737d1348ceb67f4bc12100514be172992a48a87b
Gustavo Priebe <40157345+gustavospriebe@users.noreply.github.com>
2023-06-19 01:47:50 -0300

    tailwind group trick to manipulate classes

--- components/Plans/Plans.tsx
+++ components/Plans/Plans.tsx
@@ -1,14 +1,15 @@
 export function Plans() {
     return (
         <div className="flex flex-wrap p-12">
-            <div className="card-container m-7 basis-[300px]">
-                <div className="card relative w-full font-bold">
-                    <div className="effect-hover block h-[400px] w-full border-2 border-black bg-white p-[30px] text-black no-underline transition duration-300 hover:-translate-x-7 hover:-translate-y-7 hover:border-green-principal">
-                        <div className="card--display">
-                            <h2 className="mt-[200px] text-2xl">Hot Tub Time Machine</h2>
-                            <p>oi</p>
+            <div className="m-7 basis-[300px]">
+                <div className="relative w-full font-bold">
+                    <div className="group block h-[400px] w-full rounded-md border-2 border-black bg-white p-[30px] text-black no-underline transition duration-300 hover:-translate-x-7 hover:-translate-y-7 hover:border-green-principal">
+                        <div className="group-hover:hidden">
+                            <h2 className="mt-[200px] text-2xl">
+                                Hot Tub Time 
+                            </h2>
                         </div>
-                        <div className="card--hover hidden">
+                        <div className="group-hover:block hidden">
                             <h2 className="my-5">Hot Tub Time Machine</h2>
                             <p className="font-normal">
                                 Lorem ipsum dolor sit amet, consectetur
@@ -17,60 +18,12 @@ export function Plans() {
                                 volutpat arcu sit amet elit sodales, nec
                                 volutpat velit bibendum.
                             </p>
-                            <p className="link my-5 font-bold text-green-secundary">
+                            <p className="my-5 font-bold text-green-secundary">
                                 Click to see project
                             </p>
                         </div>
                     </div>
-                    <div className="card--border absolute left-0 top-0 -z-[1] h-full w-full border-2 border-dashed border-black"></div>
-                </div>
-            </div>
-            <div className="card-container m-7 basis-[300px]">
-                <div className="card relative w-full font-bold">
-                    <div className="effect-hover block h-[400px] w-full border-2 border-black bg-white p-[30px] text-black no-underline transition duration-300 hover:-translate-x-7 hover:-translate-y-7 hover:border-green-principal">
-                        <div className="card--display">
-                            <h2 className="mt-[200px] text-2xl">Hot Tub Time Machine</h2>
-                            <p>oi</p>
-                        </div>
-                        <div className="card--hover hidden">
-                            <h2 className="my-5">Hot Tub Time Machine</h2>
-                            <p className="font-normal">
-                                Lorem ipsum dolor sit amet, consectetur
-                                adipiscing elit. Nullam at est orci. Nam
-                                molestie pellentesque mi nec lacinia. Cras
-                                volutpat arcu sit amet elit sodales, nec
-                                volutpat velit bibendum.
-                            </p>
-                            <p className="link my-5 font-bold text-green-secundary">
-                                Click to see project
-                            </p>
-                        </div>
-                    </div>
-                    <div className="card--border absolute left-0 top-0 -z-[1] h-full w-full border-2 border-dashed border-black"></div>
-                </div>
-            </div>
-            <div className="card-container m-7 basis-[300px]">
-                <div className="card relative w-full font-bold">
-                    <div className="effect-hover block h-[400px] w-full border-2 border-black bg-white p-[30px] text-black no-underline transition duration-300 hover:-translate-x-7 hover:-translate-y-7 hover:border-green-principal">
-                        <div className="card--display">
-                            <h2 className="mt-[200px] text-2xl">Hot Tub Time Machine</h2>
-                            <p>oi</p>
-                        </div>
-                        <div className="card--hover hidden">
-                            <h2 className="my-5">Hot Tub Time Machine</h2>
-                            <p className="font-normal">
-                                Lorem ipsum dolor sit amet, consectetur
-                                adipiscing elit. Nullam at est orci. Nam
-                                molestie pellentesque mi nec lacinia. Cras
-                                volutpat arcu sit amet elit sodales, nec
-                                volutpat velit bibendum.
-                            </p>
-                            <p className="link my-5 font-bold text-green-secundary">
-                                Click to see project
-                            </p>
-                        </div>
-                    </div>
-                    <div className="card--border absolute left-0 top-0 -z-[1] h-full w-full border-2 border-dashed border-black"></div>
+                    <div className="absolute left-0 top-0 -z-[1] h-full w-full rounded-md border-2 border-dashed border-black"></div>
                 </div>
             </div>
         </div>
