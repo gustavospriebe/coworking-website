4c676fcd480094df4b5a2e668d6440aeecc703fa
Gustavo Priebe <40157345+gustavospriebe@users.noreply.github.com>
2023-06-21 12:45:54 -0300

    carousel description color change

--- components/Carousel/Card.tsx
+++ components/Carousel/Card.tsx
@@ -42,7 +42,7 @@ export function CardComponent({
                         <CardTitle className="font-exotwo  font-bold">
                             {items[index].title}
                         </CardTitle>
-                        <CardDescription className="hidden font-nunitosans text-whiter/40 sm:flex">
+                        <CardDescription className="hidden font-nunitosans text-[#8c8c8c] sm:flex">
                             {items[index].description}
                         </CardDescription>
                     </CardHeader>
